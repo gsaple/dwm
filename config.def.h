7f32c13a26747de19c666754017d5342f09807a8
gsaple <yymelau@gmail.com>
2022-07-15 13:00:23 +1000

    modified tag keys

--- config.def.h
+++ config.def.h
@@ -50,9 +50,11 @@ static const Layout layouts[] = {
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	/* control which tags should be given for a FUTURE client */ \
+	/*{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} },*/ \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	/* control which tags should be given for an EXISTING client */ \
+	{ MODKEY|ControlMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
