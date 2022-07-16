b25a7cdf3c596ecca60d0fdde3e7376de6fd0d87
gsaple <yymelau@gmail.com>
2022-07-16 15:59:45 +1000

    fixborders patched

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *volume_increase[] = {"volume.sh", "up", NULL};
 static const char *volume_decrease[] = {"volume.sh", "down", NULL};
