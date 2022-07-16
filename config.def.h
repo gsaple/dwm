e27aa85e3ed157d8dd8a7148192241012cd244c3
gsaple <yymelau@gmail.com>
2022-07-17 00:20:03 +1000

    fibonacci patched

--- config.def.h
+++ config.def.h
@@ -43,11 +43,13 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+ 	{ "[@]",      spiral },
 };
 
 /* key definitions */
