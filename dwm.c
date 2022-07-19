c324d13041cb963fbb0c4dafbc911e65527a044c
gsaple <yymelau@gmail.com>
2022-07-19 22:12:45 +1000

    disable focus change on mouse move

--- dwm.c
+++ dwm.c
@@ -181,7 +181,7 @@ static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
-static void enternotify(XEvent *e);
+/*static void enternotify(XEvent *e);*/
 static void expose(XEvent *e);
 static void focus(Client *c);
 static void focusin(XEvent *e);
@@ -275,7 +275,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[ConfigureRequest] = configurerequest,
 	[ConfigureNotify] = configurenotify,
 	[DestroyNotify] = destroynotify,
-	[EnterNotify] = enternotify,
+	/*[EnterNotify] = enternotify,*/
 	[Expose] = expose,
 	[FocusIn] = focusin,
 	[KeyPress] = keypress,
@@ -819,24 +819,25 @@ drawbars(void)
 		drawbar(m);
 }
 
-void
-enternotify(XEvent *e)
-{
-	Client *c;
-	Monitor *m;
-	XCrossingEvent *ev = &e->xcrossing;
-
-	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
-		return;
-	c = wintoclient(ev->window);
-	m = c ? c->mon : wintomon(ev->window);
-	if (m != selmon) {
-		unfocus(selmon->sel, 1);
-		selmon = m;
-	} else if (!c || c == selmon->sel)
-		return;
-	focus(c);
-}
+/*void
+*enternotify(XEvent *e)
+*{
+*	Client *c;
+*	Monitor *m;
+*	XCrossingEvent *ev = &e->xcrossing;
+*
+*	if ((ev->mode != NotifyNormal || ev->detail == NotifyInferior) && ev->window != root)
+*		return;
+*	c = wintoclient(ev->window);
+*	m = c ? c->mon : wintomon(ev->window);
+*	if (m != selmon) {
+*		unfocus(selmon->sel, 1);
+*		selmon = m;
+*	} else if (!c || c == selmon->sel)
+*		return;
+*	focus(c);
+*}
+*/
 
 void
 expose(XEvent *e)
