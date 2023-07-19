f214a540a1f9767abe5c7fc06b48cb5a1b682804
gsaple <yymelau@gmail.com>
2023-07-19 21:33:07 +1000

    upstream change

--- config.def.h
+++ config.def.h
@@ -104,7 +104,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-l", "20", NULL};
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-l", "20", NULL};
 static const char *termcmd[]  = { "st", NULL };
 static const char *volume_increase[] = {"volume.sh", "up", NULL};
 static const char *volume_decrease[] = {"volume.sh", "down", NULL};
